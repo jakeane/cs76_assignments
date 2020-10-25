e06723a03a8a33ea38af4d2760a803baa4cf5d6f
Jack Keane <john.f.keane.22@dartmouth.edu>
2020-10-25 14:17:22 -0400

    init pa5

---
+++ pa5/SAT.py
@@ -0,0 +1,11 @@
+class SAT:
+
+    def __init__(self):
+        pass
+
+    def GSAT(self):
+        self.random_assignment()
+
+    def random_assignment(self):
+        for value in self.values:
+            pass
